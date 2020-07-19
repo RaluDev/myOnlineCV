2a1fc5c7d5603168b9df6881b4eccb0dc3dd6fab
Raluca Pop <popralluca@gmail.com>
2020-07-19 14:46:53 +0300

    home and about section added / nav style

--- main.js
+++ main.js
@@ -11,3 +11,20 @@ const showMenu = (toggleId, navId) => {
 }
 
 showMenu('nav-toggle', 'nav-menu');
+
+
+//remove menu on click active link
+const navLink = document.querySelectorAll('.nav__link');
+
+function linkAction() {
+    //Active link
+    navLink.forEach(link => link.classList.remove('active'));
+    this.classList.add('active');
+    //console.log(this);
+
+    //remove menu on click
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show')
+}
+navLink.forEach(link => link.addEventListener('click', linkAction));
+
